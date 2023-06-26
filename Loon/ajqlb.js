405f54c04c230490fb7022749bc73faaded624fe
huimeng12138 <95393112+huimeng12138@users.noreply.github.com>
2023-06-26 17:28:59 +0800

    Add files via upload

---
+++ Loon/ajqlb.js
@@ -0,0 +1,12 @@
+#!name=LoveChanVideoCrack
+#!desc=LoveChanVideoCrack
+#!icon=https://github.com/Toperlock/Quantumult/raw/main/icon/Doraemon/Doraemon-1057.png
+
+[Script]
+
+# > 爱酱视频破解超级至尊会员
+http-response ^https?:\/\/c\.h5aj.*\.com\/h5\/app\/api\/(tab\/module|(video|user)\/info) script-path=https://raw.githubusercontent.com/yqc007/QuantumultX/master/LoveChanVideoCrack.js, requires-body=true, timeout=60 ,tag=LoveChanVideoCrack_11
+
+[MITM]
+
+hostname = c.h5ajww.com,c.h5aj66.com,c.h5aj88.com
